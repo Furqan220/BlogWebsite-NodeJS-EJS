4f1cd48e7ad01a799ff4756161e6084c306b477c
Furqan <96830775+Furqan220@users.noreply.github.com>
2023-10-27 05:55:47 +0500

    middleware added

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 // server side code using express 
 const express = require('express');
-
+const morgan = require('morgan');
+//express app
 const app = express();
+
+//listen for requests
 app.listen(3000);
 
 //register view engines
 app.set('view engine','ejs');
+
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'))
+
+// blogs
+const blogs = [
+    {title : "Blog 1", description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit."},
+    {title : "Blog 2", description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit."},
+    {title : "Blog 3", description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit."},
+]; 
 
 // listen for requests
 app.get('/',(req,res)=>{
@@ -13,8 +28,10 @@ app.get('/',(req,res)=>{
     // res.sendFile('./view/index.html',{
     //     root:__dirname
     // })
+    
     res.render('index',{
-        title : "Home"
+        title : "Home",
+        blogs : blogs
     });
 });
 
@@ -41,5 +58,7 @@ app.use((req,res)=>{
     // res.sendFile('./view/404.html',{
     //     root:__dirname
     // })
-    res.status(404).render('404');
+    res.status(404).render('404',{
+        title : "Not Found"
+    });
 });
